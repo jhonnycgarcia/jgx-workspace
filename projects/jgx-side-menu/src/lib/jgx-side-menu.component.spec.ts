c8adb340fb0f5d5e235f19ba115e3856115667eb
Jhonny <jhonnycgarcia@gmail.com>
2024-10-30 22:38:08 -0500

    Testing del paquete

--- projects/jgx-side-menu/src/lib/jgx-side-menu.component.spec.ts
+++ projects/jgx-side-menu/src/lib/jgx-side-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JgxSideMenuComponent } from './jgx-side-menu.component';
+import { provideRouter } from '@angular/router';
 
 describe('JgxSideMenuComponent', () => {
   let component: JgxSideMenuComponent;
@@ -8,7 +9,8 @@ describe('JgxSideMenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [JgxSideMenuComponent]
+      imports: [JgxSideMenuComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
@@ -20,4 +22,27 @@ describe('JgxSideMenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call onSignIn when logout button is clicked', () => {
+    spyOn(component.onSignIn, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', false);
+
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('[data-login]') as HTMLButtonElement;
+    expect(button).toBeTruthy();
+    button.click();
+    expect(component.onSignIn.emit).toHaveBeenCalled();
+  });
+
+  it('should call onSignOut when logout button is clicked', () => {
+    spyOn(component.onSignOut, 'emit');
+    fixture.componentRef.setInput('isAuthenticated', true);
+
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('[data-logout]') as HTMLButtonElement;
+    expect(button).toBeTruthy();
+    button.click();
+    expect(component.onSignOut.emit).toHaveBeenCalled();
+  });
+
 });
